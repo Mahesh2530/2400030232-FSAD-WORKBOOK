a89472deedcfc85e9622f08a07d7ed6ef08105d1
Mahesh2530 <jothik2530@gmail.com>
2026-03-22 09:30:55 +0530

    Upto 10 experements

--- experiment-1/main.java
+++ experiment-1/main.java
@@ -1,2 +1,1 @@
-public class Main { public static void main(String[] args){ System.out.println("Hello Git"); } } 
-Feature update 
+public class Main { public static void main(String[] args){ System.out.println("Hello Git"); } }
